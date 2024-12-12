956220a7e7d6193320a3f9baf5ae719f427882df
gedesiwen <mrgoethe0126@gmail.com>
2024-12-12 20:54:00 +0800

    feature: build type file

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
 import terser from '@rollup/plugin-terser';
+import del from 'rollup-plugin-delete';
 
 export default {
   input: 'src/index.ts',
@@ -18,14 +19,19 @@ export default {
       useTsconfigDeclarationDir: true,
       tsconfigOverride: {
         compilerOptions: {
-          module: "esnext"
+          module: "esnext",
+          declaration: true, // 确保生成类型声明
+          declarationDir: 'dist', // 类型声明输出目录
+          outDir: 'dist', // 输出目录
         }
-      }
+      },
+      tsconfig: './tsconfig.json',
     }),
     terser({
       format: {
         comments: false, // 移除所有注释
       },
     }),
+    del({ targets: 'dist/*' }), // 在构建前删除 dist 目录
   ]
 };
